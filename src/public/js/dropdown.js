b13c8e513fb9c63e6fe78619c84e57ecc30d2c4c
Branden Wheeler <branden.wheeler17@gmail.com>
2022-04-03 21:02:52 -0400

    Removed cloud functions

--- src/public/js/dropdown.js
+++
@@ -1,5 +0,0 @@
-$('#dropdown1').selectmenu();
-
-for (let i = 1; i < 6; i++) {
-    $('#dropdown' + i.toString()).selectmenu();
-}
--- src/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App"
-import './public/styles/css/styles.css'
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-  );  
