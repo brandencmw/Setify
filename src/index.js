13667db404257fdae316eba368a7e89a982ef4e2
Branden Wheeler <branden.wheeler17@gmail.com>
2022-04-04 21:26:26 -0400

    Restructure folders

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App"
-import './assets/styles/css/styles.css'
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-  );  
